77ccb4779032af399a77483f10d88a9958ac734b
mateomunera <mateomunera2004@gmail.com>
2022-03-22 17:28:43 -0500

    cinee

---
+++ reserva.js
@@ -0,0 +1,10 @@
+let contenedor=document.getElementById("contenedor")
+    contenedor.addEventListener("click",function(evento){
+        if(evento.target.classList.contains("w-100")){
+            
+            evento.target.src="imagenes/chair.png"
+        }
+    })
+
+
+
